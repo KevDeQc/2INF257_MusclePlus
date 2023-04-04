185ebb4ac20b6b668b09cf6e4c36b25be3c09eb8
Tom Leyrie <Tom.leyrie@gmail.com>
2023-04-04 13:27:37 -0400

    Adding line graph but still hard coded

--- app/src/main/java/com/example/musclepluscompose/Graph.kt
+++ app/src/main/java/com/example/musclepluscompose/Graph.kt
@@ -23,13 +23,28 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.unit.toSize
 import com.example.musclepluscompose.ui.theme.Purple500
+import android.graphics.Paint
+import androidx.compose.foundation.Canvas
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.font.FontFamily
+import kotlin.math.round
+import kotlin.math.roundToInt
 
 
 @Composable
-fun Chart(
-    data: Map<Float, String>,
-    max_value: Int
+fun LineChart(
+    data: List<Pair<String, Double>> = emptyList(),
+    modifier: Modifier = Modifier
 ) {
+    val spacing = 100f
+    val graphColor = Color.Blue
+    val transparentGraphColor = remember { graphColor.copy(alpha = 0.5f) }
+    val upperValue = remember { (data.maxOfOrNull { it.second }?.plus(1))?.roundToInt() ?: 0 }
+    val lowerValue = remember { (data.minOfOrNull { it.second }?.toInt() ?: 0) }
+    val density = LocalDensity.current
 
     var expanded by remember { mutableStateOf(false) }
     var textFiledSize by remember { mutableStateOf(Size.Zero) }
@@ -53,13 +68,13 @@ fun Chart(
         Icons.Filled.KeyboardArrowDown
     }
 
-    val context = LocalContext.current
-    // BarGraph Dimensions
-    val barGraphHeight by remember { mutableStateOf(200.dp) }
-    val barGraphWidth by remember { mutableStateOf(20.dp) }
-    // Scale Dimensions
-    val scaleYAxisWidth by remember { mutableStateOf(50.dp) }
-    val scaleLineWidth by remember { mutableStateOf(2.dp) }
+    val textPaint = remember(density) {
+        Paint().apply {
+            color = android.graphics.Color.BLACK
+            textAlign = Paint.Align.CENTER
+            textSize = density.run { 12.sp.toPx() }
+        }
+    }
 
     Column(Modifier.fillMaxSize()) {
         Column(modifier = Modifier.padding(20.dp, 10.dp)) {
@@ -128,105 +143,99 @@ fun Chart(
                 }
 
             }
-            
+
         }
 
+        Spacer(
+            Modifier
+                .fillMaxWidth()
+                .weight(1f))
         Column(
-            modifier = Modifier
-                .padding(10.dp, 30.dp)
-                .fillMaxSize(),
-            verticalArrangement = Arrangement.Bottom
-        ) {
+            modifier = Modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Text(text = "Push up over time",
+                textAlign = TextAlign.Center,
+                fontSize = 20.sp,
+                fontFamily = FontFamily.Serif)
+        }
 
+        Spacer(
+            Modifier
+                .fillMaxWidth()
+                .weight(0.5f))
 
-
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(barGraphHeight),
-                verticalAlignment = Alignment.Bottom,
-                horizontalArrangement = Arrangement.Start
-            ) {
-                // scale Y-Axis
-                Box(
-                    modifier = Modifier
-                        .fillMaxHeight()
-                        .width(scaleYAxisWidth),
-                    contentAlignment = Alignment.BottomCenter
-                ) {
-                    Column(
-                        modifier = Modifier.fillMaxHeight(),
-                        verticalArrangement = Arrangement.Bottom
-                    ) {
-                        Text(text = max_value.toString())
-                        Spacer(modifier = Modifier.fillMaxHeight())
-                    }
-
-                    Column(
-                        modifier = Modifier.fillMaxHeight(),
-                        verticalArrangement = Arrangement.Bottom
-                    ) {
-                        Text(text = (max_value / 2).toString())
-                        Spacer(modifier = Modifier.fillMaxHeight(0.5f))
-                    }
-
-                }
-
-                // Y-Axis Line
-                Box(
-                    modifier = Modifier
-                        .fillMaxHeight()
-                        .width(scaleLineWidth)
-                        .background(Color.Black)
-                )
-
-                // graph
-                data.forEach {
-                    Box(
-                        modifier = Modifier
-                            .padding(start = barGraphWidth * 2f, bottom = 5.dp)
-                            .clip(CircleShape)
-                            .width(barGraphWidth)
-                            .fillMaxHeight(it.key)
-                            .background(Purple500)
-                            .clickable {
-                                Toast
-                                    .makeText(context, it.key.toString(), Toast.LENGTH_SHORT)
-                                    .show()
-                            }
+        Canvas(modifier = modifier.padding(0.dp, 0.dp, 0.dp, 20.dp)) {
+            val spacePerHour = (size.width - spacing) / data.size
+            (data.indices).forEach { i ->
+                val hour = data[i].first
+                drawContext.canvas.nativeCanvas.apply {
+                    drawText(
+                        hour.toString(),
+                        spacing + i * spacePerHour,
+                        size.height,
+                        textPaint
                     )
                 }
-
             }
 
-            // X-Axis Line
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(scaleLineWidth)
-                    .background(Color.Black)
+            val priceStep = (upperValue - lowerValue) / 5f
+            (0..5).forEach { i ->
+                drawContext.canvas.nativeCanvas.apply {
+                    drawText(
+                        round(lowerValue + priceStep * i).toInt().toString(),
+                        30f,
+                        size.height - spacing - i * size.height / 5f,
+                        textPaint
+                    )
+                }
+            }
+
+            val strokePath = Path().apply {
+                val height = size.height
+                data.indices.forEach { i ->
+                    val info = data[i]
+                    val ratio = (info.second - lowerValue) / (upperValue - lowerValue)
+
+                    val x1 = spacing + i * spacePerHour
+                    val y1 = height - spacing - (ratio * height).toFloat()
+
+                    if (i == 0) { moveTo(x1, y1) }
+                    lineTo(x1, y1)
+                }
+            }
+
+            drawPath(
+                path = strokePath,
+                color = graphColor,
+                style = Stroke(
+                    width = 2.dp.toPx(),
+                    cap = StrokeCap.Round
+                )
             )
 
-            // Scale X-Axis
-            Row(
-                modifier = Modifier
-                    .padding(start = scaleYAxisWidth + barGraphWidth * 2f + scaleLineWidth)
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.spacedBy(barGraphWidth)
-            ) {
-
-                data.values.forEach {
-                    Text(
-                        modifier = Modifier.width((barGraphWidth * 2f)),
-                        text = it,
-                        textAlign = TextAlign.Center,
-                        fontSize = 12.sp
-                    )
-                }
-
+            val fillPath = strokePath.asAndroidPath().asComposePath().apply {
+                lineTo(size.width - spacePerHour, size.height - spacing)
+                lineTo(spacing, size.height - spacing)
+                close()
             }
+
+            drawPath(
+                path = fillPath,
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        transparentGraphColor,
+                        Color.Transparent
+                    ),
+                    endY = size.height - spacing
+                )
+            )
+
         }
+
     }
 
-
 }
+
+
